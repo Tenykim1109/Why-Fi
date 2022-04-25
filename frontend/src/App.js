80b9e3501c749ec17d7bfcb92e7692d677fefaee
Minjeong Kim <lona573@gmail.com>
2022-04-26 01:24:49 +0900

    test: 배포 테스트 #2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>안녕하세요, WHY.Fi 프로젝트 배포 테스트 중입니다.</h2>
     </div>
   );
 }
