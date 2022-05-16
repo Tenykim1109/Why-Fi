f10028eb3b7dcfe84b300468c806bb3ce0d6871e
kiel <aceg1541@naver.com>
2022-05-16 14:20:35 +0900

    chore : backend entrypoint

---
+++ backend/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pip install --upgrade pip
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate --noinput
+
+gunicorn --bind 0.0.0.0:8000 config.wsgi:application --workers 4 --threads 4
