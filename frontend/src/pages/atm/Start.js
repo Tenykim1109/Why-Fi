a8b9611f9b48fb22134ab0af508ee4e5a539a132
LEMONRIME <leejsl96@gmail.com>
2022-05-17 15:48:37 +0900

    STYLE: change position of buttons

--- frontend/src/pages/atm/Start.js
+++ frontend/src/pages/atm/Start.js
@@ -2,18 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-const Button = styled.button`
-  width: 200px;
-  height: 80px;
-  background-color: #4cb5f5;
-  border-radius: 8px;
-  border: none;
-  color: white;
-  font-size: 2rem;
-  font-weight: bold;
-  cursor: pointer;
-`;
-
 const Start = () => {
   const navigate = useNavigate();
   const toTransfer = () => {
@@ -31,5 +19,22 @@ const Start = () => {
     </div>
   );
 };
+
+const Button = styled.button`
+  display: block;
+  width: 200px;
+  height: 80px;
+  background-color: #4cb5f5;
+  border-radius: 8px;
+  border: none;
+  color: white;
+  font-size: 2rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 
 export default Start;
