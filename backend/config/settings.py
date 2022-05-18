ca71f82be3ec5b443d2497702bc280ec1c39c1f9
kiel <aceg1541@naver.com>
2022-05-18 16:54:05 +0900

    fix : settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,7 +10,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(
-    os.path.join(BASE_DIR, '.env')
+    os.path.join(BASE_DIR, 'test.env')
 )
 
 DEBUG = env('DEBUG')
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
